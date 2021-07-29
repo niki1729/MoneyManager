40dd6d5a684dcf6e7b1ee7432e22d67c2b3e3238
niki1729 <nikita.michael.kiselev@gmail.com>
2021-07-29 16:34:03 +0200

    Added correct Transitions between tabs in root_screen.kv and added the checkbox "checkbox_every_month" and the function to have the possibility to add a payment that is monthly and will be taken in account at a correct date (also possible to precise it) automatically.

--- App/libs/baseclass/addingtransactionscreen.py
+++ App/libs/baseclass/addingtransactionscreen.py
@@ -12,16 +12,51 @@ class AddingTransactionScreen(MDScreen):
 
     def display_date_time(self):
         # self.ids.date_and_time.text=str(date.today())
-        self.ids.date_and_time.text = str(time.strftime("%d.%m.%y") + "   " + time.strftime("%H:%M:%S"))
+
+        self.ids.date_of_transaction_inp.text = str(time.strftime("%d.%m.%y"))
+        self.ids.time_of_transaction_inp.text = str(time.strftime("%H:%M:%S"))
+
+        self.ids.every_month_payment_date_text_field.text = str(time.strftime("%d.%m.%y"))
 
     def change_color(self):
         self.ids.welcome_label.color = randint(0, 100) / 100, randint(0, 100) / 100, randint(0, 100) / 100, 1
 
     def save_transaction(self):
-        print("saved")
-        Snackbar(text="Saved").open()
+        #
+        # ! !need to create control of the input here. Not only date and time but other things too and do it better!
+        #
+        if self.ids.checkbox_every_month.state == "normal":
+            print(
+                "saved" + self.ids.date_of_transaction_inp.text + "    " + self.ids.time_of_transaction_inp.text)
+            Snackbar(
+                text="Saved, transaction date: " + self.ids.date_of_transaction_inp.text +
+                     " transaction time: " + self.ids.time_of_transaction_inp.text).open()
+
+        else:
+            print(
+                "saved" + self.ids.date_of_transaction_inp.text + "    " + self.ids.time_of_transaction_inp.text)
+            Snackbar(
+                text="Saved, MONTHLY ON THE " + self.ids.every_month_payment_date_text_field.text
+                     + ", transaction date: " + self.ids.date_of_transaction_inp.text +
+                     " transaction time: " + self.ids.time_of_transaction_inp.text).open()
         self.parent.current = "mmapp root screen"
 
     def abort_transaction(self):
         print("ecs")
         self.parent.current = "mmapp root screen"
+
+    def every_month_payment(self, checkbox, value):
+        if checkbox.state == "down":
+            print("HI")
+            self.ids.every_month_payment_date_text_field.disabled = False
+
+        if checkbox.state == "normal":
+            self.ids.every_month_payment_date_text_field.disabled = True
+
+
+"""
+self.ids.date_and_time.text = str(time.strftime("%d.%m.%y") + "   " + time.strftime("%H:%M:%S"))
+                Label:
+                    id: date_and_time
+                    text:""
+                    font_size: 30"""
